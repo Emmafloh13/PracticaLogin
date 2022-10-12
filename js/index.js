a1a6b0c2aafac6448c707ebd352f2336158ef160
Emmafloh13 <emmafh342@gmail.com>
2022-10-12 11:44:58 -0500

    Practica login 5 video

---
+++ js/index.js
@@ -0,0 +1,15 @@
+//Declarar varibles para vincular los objetos del formlario
+
+const usuaruio = document.getElementById('user');
+const password = document.getElementById('pass');
+const formulario = document.getElementById('formlogin');
+
+//generando eventos
+formulario.addEventListener('submit'. login);
+
+//funciones a ejecutar
+function login(e){
+    e.preventDefault();
+    
+}
+
